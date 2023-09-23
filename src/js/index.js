a55ed4f4928780d9d216cf691eb44e9fba3e9adb
pashakiz <pashakiz@gmail.com>
2023-09-23 22:51:10 +0300

    done

--- src/js/index.js
+++ src/js/index.js
@@ -1,115 +1,58 @@
-import { Modal } from 'bootstrap';
-import Splide from '@splidejs/splide';
-import '@splidejs/splide/css/core';
 import '@scss/main.scss'
 
-//preloader
-const preloader = () => {
-  setTimeout(function () {
-    let preloader = document.body.querySelector('#page-preloader');
-    if (!preloader)
-      return false
-    if (!preloader.classList.contains('done'))
-      preloader.classList.add('done');
-  }, 1000);
-}
-document.addEventListener('DOMContentLoaded', preloader, false);
+const enableStylesheet = (node) => node.disabled = false;
+const disableStylesheet = (node) => node.disabled = true;
 
-//header random users
-const getRandomInt = (max) => Math.floor(Math.random() * max) + 1;
-const count = 5;
-let selections = document.body.querySelectorAll('.choose');
-selections[getRandomInt(count) - 1].style.display = 'flex';
+let themelight = document.getElementById('themelight');
+let customlight = document.getElementById('customlight');
+let themedark = document.getElementById('themedark');
+let customdark = document.getElementById('customdark');
+let themeSwitchIcon = document.querySelector('.cssmode__icon');
+let themeSwitchIconSun = themeSwitchIcon.querySelector('.icon-sun');
+let themeSwitchIconMoon = themeSwitchIcon.querySelector('.icon-moon');
+let userTheme = localStorage.getItem('userTheme');
 
-//form
-const form = document.querySelector('.form');
-const formBtn = document.querySelector('.form__btn');
-const formInput = document.querySelector('.form input.form-control');
-const messageTrue = document.querySelector('.form .form__response.form__response_true');
-const messageFalse = document.querySelector('.form .form__response.form__response_false');
-
-const formValidate = () => {
-  let error = false;
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-  if (formInput.value == '')
-    error = true;
-
-  if (!emailRegex.test(formInput.value))
-    error = true;
-
-  if (error) {
-    messageTrue.style.display = 'none';
-    messageFalse.style.display = 'block';
-    formInput.classList.add('form-control_error');
-    return false
-  }
-
-  formSend();
-}
-const formSend = () => {
-  console.log('formSend');
-  formInput.classList.remove('form-control_error');
-  messageFalse.style.display = 'none';
-  messageTrue.style.display = 'block';
+const turnLightTheme = () => {
+  document.querySelector('html').classList.remove('themedark');
+  document.querySelector('html').classList.add('themelight');
+  themeSwitchIconSun.style.display = 'none';
+  themeSwitchIconMoon.style.display = 'inline';
+  disableStylesheet(themedark);
+  disableStylesheet(customdark);
+  enableStylesheet(themelight);
+  enableStylesheet(customlight);
+  localStorage.setItem('userTheme', 'light');
 }
 
-if (!!form)
-  formBtn.addEventListener('click', formValidate, false);
-
-//open all (links)
-const moreLinksBtn = document.querySelector('.category__btn');
-const showMoreLinks = (e) => {
-  const hiddenLinks = document.querySelectorAll('.category__hide');
-  hiddenLinks.forEach(links => links.classList.remove('d-none', 'd-xl-block'));
-  e.target.style.display = 'none';
-}
-moreLinksBtn.addEventListener('click', showMoreLinks, false);
-
-// Splidejs (cursor customisation)
-const splides = document.querySelectorAll('.splide');
-
-const splideIsDrag = (e) => {
-  if (e.target.closest('.splide') === null)
-    return false
-  e.target.closest('.splide').classList.add('is-grab');
+const turnDarkTheme = () => {
+  document.querySelector('html').classList.remove('themelight');
+  document.querySelector('html').classList.add('themedark');
+  themeSwitchIconSun.style.display = 'inline';
+  themeSwitchIconMoon.style.display = 'none';
+  disableStylesheet(themelight);
+  disableStylesheet(customlight);
+  enableStylesheet(themedark);
+  enableStylesheet(customdark);
+  localStorage.setItem('userTheme', 'dark');
 }
 
-const splideIsntDrag = (e) => {
-  if (e.target.closest('.splide') === null)
-    return false
-  e.target.closest('.splide').classList.remove('is-grab');
+if (userTheme) {
+  if (userTheme == 'dark')
+    turnDarkTheme();
+  if (userTheme == 'light')
+    turnLightTheme();
+} else {
+  if (document.querySelector('html').classList.contains('themedark'))
+    turnDarkTheme();
+  if (document.querySelector('html').classList.contains('themelight'))
+    turnLightTheme();
 }
 
-if (!!splides) {
-  splides.forEach(el => el.addEventListener('mousedown', splideIsDrag, false));
-  splides.forEach(el => el.addEventListener('mouseup', splideIsntDrag, false));
+const themeSwitch = (e) => {
+  if (document.querySelector('html').classList.contains('themedark'))
+    turnLightTheme();
+  else
+    turnDarkTheme();
 }
 
-// Splidejs
-// https://github.com/Splidejs/splide
-
-if (document.querySelector('.splide_vitems') !== null) {
-  new Splide( '.splide_vitems', {
-    type       : 'loop',
-    arrows     : false,
-    //arrowPath  : 'M0,0H40V40H0Z',
-    autoHeight : true,
-    autoWidth  : true,
-    perPage    : 1,
-    gap        : '18px',
-    //fixedWidth : '525px',
-    mediaQuery : 'min',
-    breakpoints: {
-      992: {
-        gap    : '18px',
-      },
-      1200: {
-        gap        : '20px',
-      },
-      1400: {
-        gap        : '47px',
-      },
-    },
-  } ).mount();
-}
+themeSwitchIcon.addEventListener('click', themeSwitch, false);
