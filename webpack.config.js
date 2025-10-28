5fc3983ed95eddfc2414340dbfc21aff0940d568
pashakiz <pashakiz@gmail.com>
2025-10-28 22:30:01 +0300

    manage meta info (title, description, keywords)

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const fs = require('fs');
 const webpack = require('webpack');
+const pagesConfig = require('./pages-config');
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
@@ -16,6 +17,14 @@ function generateHtmlPlugins(templateDir) {
     const parts = item.split('.');
     const name = parts[0];
     const extension = parts[1];
+
+    // Берем конфиг для текущей страницы из pages-config.js
+    const pageConfig = pagesConfig[name] || {
+      title: 'Психолог Павел Кизернис',
+      description: 'Павел Кизернис - психолог, гештальт-терапевт в Донецке и онлайн.',
+      keywords: 'психотерапевт онлайн, психотерапевт в Донецке, гешталь-терапевт онлайн, гешталь-терапевт в Донецке, психолог в Донецке, семейный психолог в Донецке'
+    };
+
     return new HtmlWebpackPlugin({
       filename: `${name}.html`,
       template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
@@ -23,6 +32,11 @@ function generateHtmlPlugins(templateDir) {
         collapseWhitespace: isProd
       },
       inject: true,
+      templateParameters: { // Передаем переменные напрямую в шаблон
+        title: pageConfig.title,
+        description: pageConfig.description,
+        keywords: pageConfig.keywords
+      }
     })
   })
 }
